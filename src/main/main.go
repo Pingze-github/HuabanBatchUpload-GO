9791ef6c20b407512c907bec2325b68556d69565
wangshunzhang <wangshunzhang@henhaoji.com>
2017-10-23 21:16:16 +0800

    并发控制

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"flag"
 	"os"
+	"time"
+	"reflect"
 )
 
 var (
@@ -33,10 +35,60 @@ func usage() {
 	`)
 }
 
+func goTasks(task func(param interface{}), data []interface{}, limit int) {
+	// goroutine 协程
+	// channel 信道
+	// 死锁 deadlock
+
+	fcount := 0
+	limit = 3
+	channel := make(chan int, limit)
+	quit := make(chan int)
+	taskWrapper := func(param interface{}) {
+		channel <- 1
+		task(param)
+		<-channel
+		fcount ++
+		if fcount == len(data) {
+			<-quit
+		}
+	}
+	var i int
+	for i = 0; i < len(data); i++ {
+		go taskWrapper(data[i])
+	}
+	quit <- 1
+}
+
+func _foo(param int) {
+	fmt.Println(param)
+	time.Sleep(1000000000) //1s
+}
+
+func foo(param interface{}) {
+	typ := reflect.TypeOf(param).Kind()
+	fmt.Println(typ)
+	// TODO 通过反射获取的类型字符串或Kind，并不能用来更改interface{}变量类型
+	// TODO 不可能将interface{}参数向指定类型参数的函数转化，反过来可以
+	// TODO 可以通过switch遍历进行有限种类的互相转化
+	_foo(param.(int))
+}
+
 func main() {
 	flag.Parse()
 	if help {
 		flag.Usage()
 	}
 	fmt.Println(acct, pass, board)
+
+	// TODO 建立一个有效的并发控制工具
+	// 1、可以定制要执行的任务
+	// 2、可以定制要执行的数据
+	// 3、可以定制并发数
+	// 4、可以捕获到单个任务完成，并获得返回值
+	// 5、可以在全部完成时获得返回值(选择性)
+	data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// FIXME 使用此函数进行并发控制，必须让任务函数参数、数组类型都为interface{}。这使类型检查无效，不该这样
+	goTasks(foo, data, 3)
 }
