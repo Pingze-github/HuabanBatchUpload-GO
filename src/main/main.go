265e3baa2235909ceaefe0de8121bc5ada9bcc29
wangshunzhang <wang719695@gmail.com>
2017-10-24 00:09:26 +0800

    a test

--- src/main/main.go
+++ src/main/main.go
@@ -34,12 +34,7 @@ func usage() {
 	`)
 }
 
-type TaskDataArr interface {
-	Len() int
-	Get(int) interface{}
-}
-
-func goTasks(task func(param interface{}), dataArr TaskDataArr, limit int) {
+func goTasks(task func(param Param), paramArr ParamArr, limit int) {
 	// goroutine 协程
 	// channel 信道
 	// 死锁 deadlock
@@ -48,41 +43,37 @@ func goTasks(task func(param interface{}), dataArr TaskDataArr, limit int) {
 	limit = 3
 	channel := make(chan int, limit)
 	quit := make(chan int)
-	taskWrapper := func(param interface{}) {
+	taskWrapper := func(param Param) {
 		channel <- 1
 		task(param)
 		<-channel
 		fcount ++
-		if fcount == dataArr.Len() {
+		if fcount == paramArr.Len() {
 			<-quit
 		}
 	}
 	var i int
-	for i = 0; i < dataArr.Len(); i++ {
-		go taskWrapper(dataArr.Get(i))
+	for i = 0; i < paramArr.Len(); i++ {
+		go taskWrapper(paramArr.Get(i))
 	}
 	quit <- 1
 }
 
-func _foo(param int) {
-	fmt.Println(param)
+func foo(param Param) {
+	fmt.Println(param.url)
 	time.Sleep(1000000000) //1s
 }
 
-func foo(param interface{}) {
-	// TODO 通过反射获取的类型字符串或Kind，并不能用来更改interface{}变量类型
-	// TODO 不可能将interface{}参数向指定类型参数的函数转化，反过来可以
-	// TODO 可以通过switch遍历进行有限种类的互相转化
-	// TODO 可以通过实现接口来约束类型
-	_foo(param.(int))
-}
-
-type IntArr []int
-func (arr IntArr) Len() int {
+type ParamArr []Param
+func (arr ParamArr) Len() int {
 	return len(arr)
 }
-func (arr IntArr) Get(index int) interface{} {
+func (arr ParamArr) Get(index int) Param {
 	return arr[index]
+}
+
+type Param struct {
+	url string
 }
 
 func main() {
@@ -100,8 +91,14 @@ func main() {
 	// 5、可以在全部完成时获得返回值(选择性)
 	//data := []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	dataArr := IntArr{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var paramArr ParamArr
+	paramArr = append(paramArr, Param{url: "http://www.baidu.com"})
+	paramArr = append(paramArr, Param{url: "http://www.baidu.com"})
+	paramArr = append(paramArr, Param{url: "http://www.baidu.com"})
+	paramArr = append(paramArr, Param{url: "http://www.baidu.com"})
+	paramArr = append(paramArr, Param{url: "http://www.baidu.com"})
+	paramArr = append(paramArr, Param{url: "http://www.baidu.com"})
 
 	// FIXME 使用此函数进行并发控制，必须让任务函数参数、数组类型都为interface{}。这使类型检查无效，不该这样
-	goTasks(foo, dataArr, 3)
+	goTasks(foo, paramArr, 3)
 }
